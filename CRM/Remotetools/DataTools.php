e4d774717bfe25556af4550a0296f04556fa6a1d
B. Endres <endres@systopia.de>
2021-03-12 18:39:50 +0100

    added id restriction tools

--- CRM/Remotetools/DataTools.php
+++ CRM/Remotetools/DataTools.php
@@ -125,4 +125,86 @@ class CRM_Remotetools_DataTools {
             $request_data['options']['sort'] = $sorting_string;
         }
     }
+
+    /**
+     * Get the current restriction of IDs
+     *
+     * Remark: this returns
+     *   null  if not set
+     *   fail  if couldn't be parsed
+     *   array with a list of IDs, if everything's fine
+     *
+     * @param $request array
+     *   the request
+
+     * @param $field_name string
+     *   name of the field, default is 'id'
+     *
+     * @return array|null|string
+     *   list of requested IDs
+     */
+    public static function getIDs($request, $field_name = 'id')
+    {
+        if (isset($request[$field_name])) {
+            $id_param = $request[$field_name];
+            if (is_string($id_param)) {
+                // this is a single integer, or a list of integers
+                $id_list = explode(',', $id_param);
+                return array_map('intval', $id_list);
+
+            } else if (is_array($id_param)) {
+                // this is an array. we can deal with the 'IN' => [] notation
+                if (count($id_param) == 2) {
+                    if (strtolower($id_param[0]) == 'in' && is_array($id_param[1])) {
+                        // this should be a list of IDs
+                        return array_map('intval', $id_param[1]);
+                    }
+                }
+            }
+
+            // if we get here, we couldn't parse it
+            \Civi::log()->debug("DataTools.getIDs: couldn't parse '{$field_name}' parameter: " . json_encode($id_param));
+            return 'fail';
+
+        } else {
+            // 'id' field not set
+            return null;
+        }
+    }
+
+    /**
+     * Restrict the query parameter to the given IDs.
+     *  Existing restrictions will be taken into account (intersection)
+     *
+     * @param $request array
+     *   the request
+     *
+     * @param array $ids
+     *   list of event IDs
+     *
+     * @param $field_name string
+     *   name of the field, default is 'id'
+     */
+    public static function restrictToIds(&$request, $ids, $field_name = 'id')
+    {
+        if (empty($ids)) {
+            // this basically means: restrict to empty set:
+            $request[$field_name] = 0;
+        } else {
+            $current_restriction = self::getIDs($request, $field_name);
+            if ($current_restriction === null) {
+                // no restriction set so far
+                $request[$field_name] = ['IN' => $ids];
+
+            } else if (is_array($current_restriction)) {
+                // there is a restriction -> intersect
+                $intersection = array_intersect($current_restriction, $ids);
+                $request[$field_name] = ['IN' => $intersection];
+
+            } else {
+                // something's wrong here
+                \Civi::log()->debug("DataTools.restrictToIds: couldn't restrict '{$field_name}' parameter: " . json_encode($current_restriction));
+            }
+        }
+    }
 }
